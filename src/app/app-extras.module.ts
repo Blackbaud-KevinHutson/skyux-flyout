4d355d354ce3c82e9b320d3b16c6fcb73130e375
Blackbaud-SteveBrush <steve.brush@blackbaud.com>
2018-11-13 11:51:38 -0500

    Cleanup

--- src/app/app-extras.module.ts
+++ src/app/app-extras.module.ts
@@ -7,19 +7,24 @@ import {
 } from '@angular/platform-browser/animations';
 
 import {
+  SkyModalModule
+} from '@skyux/modals/modules/modal';
+
+import {
   SkyDropdownModule
 } from '@skyux/popovers';
 
 import {
-  SkyFlyoutModule,
-  SkyFlyoutService
+  SkyFlyoutModule
 } from './public';
 
 import {
   FlyoutDemoComponent
 } from './visual/flyout/flyout-demo.component';
-import { SkyModalModule } from '@skyux/modals/modules/modal';
-import { SkyFlyoutModalDemoComponent } from './visual/flyout/flyout-modal.component';
+
+import {
+  SkyFlyoutModalDemoComponent
+} from './visual/flyout/flyout-modal.component';
 
 @NgModule({
   imports: [
